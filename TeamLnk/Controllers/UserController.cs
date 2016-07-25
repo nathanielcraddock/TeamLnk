10ac5d58eb08a05fb04e9cc12058ab7da17ffd76
Nathan Craddock <Nathaniel.Craddock@outlook.com>
2016-07-25 01:25:43 -0400

    Created User View, Model, and Controller.

---
+++ TeamLnk/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeamLnk.Models;
+
+namespace TeamLnk.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult Index()
+        {
+            IEnumerable<User> users = new List<User>()
+            {
+                new User { FName = "Vincent", LName = "Saelzler", Email = "v@e.com", Password = "SOEASY" },
+                new User { FName = "Nathan", LName = "Craddock", Email = "n@e.com", Password = "SOEASY1" }
+            };
+            return View(users);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ TeamLnk/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeamLnk.Models
+{
+    public class User
+    {
+        [Key]
+        Guid Id { get; set; }
+        [Required]
+        public string FName { get; set; }
+        [Required]
+        public string LName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        
+    }
+}
